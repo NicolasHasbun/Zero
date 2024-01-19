99a998c95905e404897d43f88891b89a13de394c
NicolasHasbun <Ni.Hasbun.Molina@gmail.com>
2024-01-19 05:08:40 -0300

    Prueba

---
+++ src/Database/firebaseConfig.js
@@ -0,0 +1,6 @@
+export const realtime_database_url =
+  "https://comision-8c5bf-default-rtdb.firebaseio.com/";
+
+export const api_key = "AIzaSyAY3pk6QT1FUYInsBpG-kT9mLlHqu2f-Dg";
+
+export const google_maps_api_key = "AIzaSyD8oq6_0Gnfw34oU-1R4krgABk02VQNqTA";
